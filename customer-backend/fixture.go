e7d3f566a5b33c7306f46993cce4bea64ac0d602
LoriStarkDDSahrpcsjerry <m_istrova_47777916@rambler.ru>
2025-12-04 19:54:04 +0800

    add deploy.ts, netlify.toml, fixture.go

---
+++ customer-backend/fixture.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\customer-backend\fixture.go
+// Generated at: 2025-10-13T13:07:23.671Z
---
+++ report/controller-endpoint.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\report\controller-endpoint.go
+// Generated at: 2025-10-13T13:07:23.668Z
